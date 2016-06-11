43c087aef172b89cd36af43ab87fae0f04ad37c0
Denis Lavrov <bahus.vel@gmail.com>
2016-06-11 14:54:44 +1200

    trimmed off the naughty data at the end, and some graphs for the presentation

--- stl_predictor.R
+++ stl_predictor.R
@@ -1,8 +1,8 @@
 library(forecast)
 
-metric_name <- "netlink.eth0.if_octets.rx.csv"
+metric_name <- "cpu.0.cpu.idle.value.csv"
 data_end <- 8640
 
-stl_series <- ts(resampled[1:data_end, metric_name], frequency = 1440)
+stl_series <- ts(final[1:data_end, metric_name], frequency = 1440)
 stl_fit <- stl(x = stl_series, s.window = "per")
 plot(forecast(stl_fit))
