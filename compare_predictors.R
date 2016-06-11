ac6f0bf55902e3606e60550df55d34000609d2fa
Denis Lavrov <bahus.vel@gmail.com>
2016-06-12 10:25:39 +1200

    added results and setup for each metric of interest

--- compare_predictors.R
+++ compare_predictors.R
@@ -1,15 +1,21 @@
 library(forecast)
 
-metric_name <- "memory.memory.free.value.csv"
+metric_name <- "netlink.eth0.if_octets.rx.csv"
 
 full_series <- ts(final[[metric_name]], frequency = 1440)
 train_series <- window(full_series, start=1,end=4)
 test_series <- window(full_series, start=4, end=6)
 
-fit_result <- stl(x = train_series, s.window = "per", robust = TRUE)
-forecast_results <- forecast(fit_result)
-
+# Holt Winters
+fit_result <- HoltWinters(train_series)
+forecast_results <- forecast.HoltWinters(fit_result, h = 2880)
 plot(forecast_results)
 lines(test_series)
+accuracy(forecast_results, test_series)
 
+#STL
+fit_result <- stl(x = train_series, s.window = "per", robust = TRUE)
+forecast_results <- forecast(fit_result)
+plot(forecast_results)
+lines(test_series)
 accuracy(forecast_results, test_series)
--- nn_predictor.R
+++ nn_predictor.R
@@ -1,14 +1,14 @@
 library(forecast)
 
-metric_name <- "df.root.df_complex.free.value.csv"
+metric_name <- "cpu.0.cpu.idle.value.csv"
 
 full_series <- ts(final[[metric_name]], frequency = 1440)
 train_series <- window(full_series, start=1,end=4)
 test_series <- window(full_series, start=4, end=6)
-fit_result <- nnetar(train_series, p = 2)
 
-forecast_results <- forecast(fit_result)
+fit_result <- nnetar(train_series)
+forecast_results <- forecast(fit_result, p = 1)
+
 plot(forecast_results)
-lines(test_series)
-
+#lines(test_series)
 accuracy(forecast_results, test_series)
