e3c3d4b440c64077460a0a03f744dbf5517d57e6
Denis Lavrov <bahus.vel@gmail.com>
2016-06-17 15:14:21 +1200

    neural networks for cpu

--- nn_predictor.R
+++ nn_predictor.R
@@ -1,14 +1,20 @@
 library(forecast)
 
-metric_name <- "cpu.0.cpu.idle.value.csv"
+metric_name <- "cpu.1.cpu.idle.value.csv"
 
 full_series <- ts(final[[metric_name]], frequency = 1440)
 train_series <- window(full_series, start=1,end=4)
 test_series <- window(full_series, start=4, end=6)
 
-fit_result <- nnetar(train_series)
-forecast_results <- forecast(fit_result, p = 1)
+fit_result <- nnetar(train_series, p = 40)
+forecast_results <- forecast(fit_result)
 
+jpeg(paste("results/", metric_name, "_nn.jpeg", sep = ""))
 plot(forecast_results)
 #lines(test_series)
-accuracy(forecast_results, test_series)
+dev.off()
+
+
+test_accuracy <- accuracy(forecast_results, test_series)
+test_accuracy
+write.table(test_accuracy, file = paste("results/", metric_name, "_nn.csv", sep = ""), sep = ",")
