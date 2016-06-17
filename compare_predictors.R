d6105f8d271208c9d976ad7853a73810111df571
Denis Lavrov <bahus.vel@gmail.com>
2016-06-17 14:28:32 +1200

    added output to predictors

--- compare_predictors.R
+++ compare_predictors.R
@@ -1,21 +1,35 @@
 library(forecast)
 
-metric_name <- "netlink.eth0.if_octets.rx.csv"
+metric_name <- "cpu.1.cpu.idle.value.csv"
 
 full_series <- ts(final[[metric_name]], frequency = 1440)
 train_series <- window(full_series, start=1,end=4)
 test_series <- window(full_series, start=4, end=6)
 
 # Holt Winters
-fit_result <- HoltWinters(train_series)
-forecast_results <- forecast.HoltWinters(fit_result, h = 2880)
+postfix <- "_holt"
+
+fit_result <- HoltWinters(train_series)                         # fit parameters
+forecast_results <- forecast.HoltWinters(fit_result, h = 2880)  # forecast parameters
+
+jpeg(paste("results/", metric_name, postfix, ".jpeg", sep = ""))
 plot(forecast_results)
 lines(test_series)
-accuracy(forecast_results, test_series)
+dev.off()
+
+result_accuracy <- accuracy(forecast_results, test_series)
+write.table(result_accuracy, file = paste("results/", metric_name, postfix, ".csv", sep = ""), sep = ",")
 
 #STL
-fit_result <- stl(x = train_series, s.window = "per", robust = TRUE)
-forecast_results <- forecast(fit_result)
+postfix <- "_stl"
+
+fit_result <- stl(x = train_series, s.window = "per")         # fit parameters
+forecast_results <- forecast(fit_result)                      # forecast parameters
+
+jpeg(paste("results/", metric_name, postfix,".jpeg", sep = ""))
 plot(forecast_results)
 lines(test_series)
-accuracy(forecast_results, test_series)
+dev.off()
+
+result_accuracy <- accuracy(forecast_results, test_series)
+write.table(result_accuracy, file = paste("results/", metric_name, postfix,".csv", sep = ""), sep = ",")
--- stl_predictor.R
+++ stl_predictor.R
@@ -7,9 +7,13 @@ train_series <- window(full_series, start=1,end=4)
 test_series <- window(full_series, start=4, end=6)
 
 fit_result <- stl(x = train_series, s.window = "per", robust = TRUE)
-forecast_results <- forecast(fit_result)
+forecast_results <- forecast(fit_result, method = "rwdrift")
 
+jpeg(paste("results/", metric_name, "_tuned_stl.jpeg", sep = ""))
 plot(forecast_results)
 lines(test_series)
+dev.off()
 
-accuracy(forecast_results, test_series)
+test_accuracy <- accuracy(forecast_results, test_series)
+test_accuracy
+write.table(test_accuracy, file = paste("results/", metric_name, "_tuned_stl.csv", sep = ""), sep = ",")
